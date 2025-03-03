5d6df2895ebb4052618bf09ac364ab6a44ca16ee
Uncode-Alpha <geimsyael@hotmail.com>
2025-03-03 03:01:23 -0600

    Setup other tests and drivers
    TODO comment added

--- src/test/java/googleExcercise/googleKaggle.java
+++ src/test/java/googleExcercise/googleKaggle.java
@@ -4,11 +4,13 @@ import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import com.aventstack.extentreports.reporter.configuration.Theme;
-
 import java.io.File;
-
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
@@ -18,6 +20,7 @@ import org.testng.annotations.BeforeTest;
 
 public class googleKaggle {
     public static WebDriver driver;
+    public static WebDriverWait wait;
     public ExtentSparkReporter sparkReporter;
     public ExtentReports extent;
     public ExtentTest logger;
@@ -34,11 +37,29 @@ public class googleKaggle {
         sparkReporter.config().setReportName("Automation Test Results");
     }
     @BeforeMethod
+    public void setup(){
+        //Here we initialize the driver
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 10);    
+    }
     @Test
+    public void googleSearchTest(){
+        setup();
+        driver.get("https://www.google.com");
+        driver.manage().window().maximize();
+        wait.until(ExpectedConditions.elementToBeClickable(By.className("q")));
+        driver.findElement(By.className("q")).sendKeys("Kaggle");
+        System.out.print("----------- Keys sent ----------");
+    
+    }
     @AfterMethod
+    public void tearDown(){
+        driver.quit();
+    }
+    
     @AfterTest
     public void afterTest(){
         //The after test method works for flushing and closing any driver or 
-        extend.flush();
+        extent.flush();
     }
 }
