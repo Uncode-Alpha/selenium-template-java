6c57563d415841700fb2006f855f0be1ab80d205
Uncode-Alpha <geimsyael@hotmail.com>
2025-03-19 23:43:50 -0600

    Automation of Kaggle WebPage

--- src/main/java/utils/SeleniumUtils.java
+++ src/main/java/utils/SeleniumUtils.java
@@ -1,10 +1,18 @@
 package utils;
 
+import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.function.Function;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -17,7 +25,8 @@ import org.openqa.selenium.support.ui.WebDriverWait;
  *
  */
 public class SeleniumUtils {
-	private static final Duration DEFAULT_WAIT_TIME = Duration.ofSeconds(10);
+	private static final String RESULT_IMAGE = "/Users/josueaguirre/Documents/GitHub/selenium-template-java/output/screenshots/";
+	private static final Duration DEFAULT_WAIT_TIME = Duration.ofSeconds(25);
 	
 	/**
 	 * Try to find an element in the page by xpath. If the element is not found,
@@ -192,5 +201,76 @@ public class SeleniumUtils {
 	public <T> T waitUntil(WebDriver driver, Function<? super WebDriver, T> condition, Duration timeOutInSeconds) {
 		WebDriverWait wait = new WebDriverWait(driver, timeOutInSeconds);
 		return wait.until(condition);
+	}
+
+	/**
+	 * Method to open specific url, click and send keys in the search box
+	 * 
+	 * @param driver
+	 * @param url
+	 * @param xpath
+	 * @param keys
+	 */
+	public static void openPageAndSendKeysByXpath(WebDriver driver, String url, String xpath, String keys){
+		if(driver == null||url == null||url.isEmpty()||xpath == null||xpath.isEmpty()||keys == null||keys.isEmpty()){
+			throw new IllegalArgumentException("Parameters are null or empty in openPageAndSendKeys method");
+		}
+		try{
+			driver.get(url);
+			driver.manage().window().maximize();
+			WebElement searchBox = waitForElementToBeVisible(driver, xpath);
+			searchBox.sendKeys(keys);
+			searchBox.sendKeys(Keys.ENTER);
+			System.out.println("----------- Keys sent ----------");
+		}catch(NoSuchElementException e){
+			System.out.println("Element was not found in  openPageAndSendKeys method");
+		}catch(TimeoutException e){
+			System.out.println("Timeout exception in openPageAndSendKeys method, waited for: "+DEFAULT_WAIT_TIME+" seconds");
+		}catch(Exception e){
+			System.out.println("Error in openPageAndSendKeys method");
+		}
+	}
+	/**
+	 * Method to take a screenshot with png extension, saved to default path
+	 * @param driver
+	 * @param screenshotName
+	 */
+	public static void takeScreenshotUtil(WebDriver driver, String screenshotName){
+		try{
+            //Convert web driver object to TakeScreenshot
+            TakesScreenshot scrShot =((TakesScreenshot)driver);
+            //Call getScreenshotAs method to create image file
+            File SrcFile=scrShot.getScreenshotAs(OutputType.FILE);
+            //Move image file to new destination
+            File DestFile=new File(RESULT_IMAGE+screenshotName+".png");
+            //Copy file at destination
+            FileUtils.copyFile(SrcFile, DestFile);
+            System.out.println("Screenshot saved at: " + DestFile.getAbsolutePath());
+        }catch(IOException e){
+            System.out.println("Error taking screenshot");
+            throw new RuntimeException();
+        }
+	}
+	/**
+	 * Method to check if the URL contains a specific text
+	 * @param driver
+	 * @param text
+	 * @return
+	 */
+	public static boolean urlContainsText(WebDriver driver,String text){
+		boolean flag=false;
+		if (driver == null || text == null || text.isEmpty()) {
+			throw new IllegalArgumentException("Driver or text parameter is null or empty in urlContainsText method");
+		}
+		try{
+			WebDriverWait wait = new WebDriverWait(driver, DEFAULT_WAIT_TIME);
+			flag=wait.until(ExpectedConditions.urlContains(text.toLowerCase()));
+			System.out.println("----------- URL contains following text ----------"+"\n"+text);
+		} catch (TimeoutException e) {
+			System.out.println("Timeout while waiting for URL to contain text: " +"\n"+text);
+		} catch (Exception e) {
+			System.out.println("Unexpected error in urlContainsText method: " +"\n"+ e.getLocalizedMessage());
+		}
+		return flag;
 	}
 }
--- src/test/java/googleExcercise/googleKaggle.java
+++ src/test/java/googleExcercise/googleKaggle.java
@@ -1,26 +1,27 @@
 package googleExcercise;
+
 //Imports
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import com.aventstack.extentreports.reporter.configuration.Theme;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
-
+import utils.SeleniumUtils;
 import java.io.File;
 import java.time.Duration;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 //Declare static constants to be used, including paths for testData
 
@@ -30,6 +31,20 @@ public class googleKaggle {
     public ExtentSparkReporter sparkReporter;
     public ExtentReports extent;
     public ExtentTest logger;
+
+    //TODO: create and move to ENUM
+    public static final String URL_GOOGLE = "https://www.google.com";
+    public static final String URL_NASA = "https://api.nasa.gov";
+    public static final String URL_KAGGLE = "https://www.kaggle.com";
+    public static final String KAGGLE_SEARCH_BAR = "//*[@id=\":r5:\"]";
+
+    String modelingXpath = "//span[contains(text(),'Preparing to modeling')]";
+    String modelSectionXpath = "//*[@id='4.-Preparing-to-modeling-']";
+    String conclusionXpath = "//*[@aria-label='7. Conclusion , 10 of 10']";
+    String keys = "Test Automation";
+    String first_result_xpath = "//*[@id=\"results\"]/ul//li[1]";
+    String screenshotName = "KaggleFirstResult";
+    public static final String FINAL_URL_TEXT= "heart-disease";
 
     @BeforeTest
     public void beforeTestMethod(){
@@ -49,20 +64,31 @@ public class googleKaggle {
         // Add Chrome options
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
-
         //Here we initialize the driver
         driver = new ChromeDriver();
-        wait = new WebDriverWait(driver, Duration.ofSeconds(10));    
+        wait = new WebDriverWait(driver, Duration.ofSeconds(25));
     }
     @Test
-    public void googleSearchTest(){
-        setup();
-        driver.get("https://www.google.com");
-        driver.manage().window().maximize();
-        wait.until(ExpectedConditions.elementToBeClickable(By.className("q")));
-        driver.findElement(By.className("q")).sendKeys("Kaggle");
-        System.out.print("----------- Keys sent ----------");
-    
+    public void googleSearchTest() throws InterruptedException{
+        boolean flag;
+        SoftAssert softAssert = new SoftAssert();
+        Actions actions = new Actions(driver);
+        //Open Kaggle and send keys
+        SeleniumUtils.openPageAndSendKeysByXpath(driver,URL_KAGGLE,KAGGLE_SEARCH_BAR,keys);
+        //Select first result
+        WebElement firstResult = SeleniumUtils.waitForElementToBeVisible(driver, first_result_xpath);
+        System.out.println("----------- Landed on results page ----------");
+        firstResult.click();
+        System.out.println("----------- Clicked on first result ----------");
+        //Landed on result after clicking
+        softAssert.assertTrue(SeleniumUtils.urlContainsText(driver, FINAL_URL_TEXT));
+        WebElement modelingLink = SeleniumUtils.waitForElementToBeClickable(driver, modelingXpath);
+        actions.moveToElement(SeleniumUtils.findElement(driver, conclusionXpath)).perform();
+        wait.until(ExpectedConditions.visibilityOf(modelingLink));
+        modelingLink.click();
+        System.out.println("----------- Clicked on modeling link ----------");
+        wait.until(ExpectedConditions.visibilityOf(SeleniumUtils.findElement(driver, modelSectionXpath)));
+        SeleniumUtils.takeScreenshotUtil(driver,screenshotName);
     }
     @AfterMethod
     public void tearDown(){
